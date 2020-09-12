2ce5034d1b0fede4dd2c34264fef32a0ec69ee78
mchadid <mchadidarchid@gmail.com>
2020-09-13 00:05:19 +0200

    docs agregado

---
+++ docs/precache-manifest.269593724f1f3e549d0382071d1f7617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aada1a935dc870509ac673425755bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6259eb0f1ac55c579564",
+    "url": "/static/css/main.74314b1e.chunk.css"
+  },
+  {
+    "revision": "fec7d0dbbf1ea5a1db01",
+    "url": "/static/js/2.299a3198.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.299a3198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6259eb0f1ac55c579564",
+    "url": "/static/js/main.b4976ab0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
